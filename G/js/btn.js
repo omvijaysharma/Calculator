15fdb80ca812f6ed79d8c5e9ee1dcc27fe753a00
Vijay Sharma <157729319+omvijaysharma@users.noreply.github.com>
2025-11-11 12:53:14 +0530

    Update btn.js

--- G/js/btn.js
+++ G/js/btn.js
@@ -1,8 +1,33 @@
+function createButtons(container, list) {
+    if (!container) {
+        console.error("Container element not found!");
+        return;
+    }
+    list.forEach(data => {
+        const button = document.createElement('div');
+        button.classList.add('app-button');
+        let src = applyCipher(data.imageSrc,num)
+        button.style.backgroundImage = `url('${src}')`;
+        
+        if (typeof data.click === 'function') {
+            button.addEventListener('click', data.click); 
+        } else if (typeof data.click === 'string') {
+             button.setAttribute('onclick', data.click);
+        }
+        
+        const text = document.createElement('p');
+        text.textContent = data.text;
+        text.classList.add('button-text');
+        
+        button.appendChild(text);
+        container.appendChild(button);
+    });
+}
 const anime = document.querySelector(".anime");
 const cartoon = document.querySelector(".cartoon");
 const real = document.querySelector(".real");
 
-const btn = [
+const pic = [
     {
         imageSrc: "AMMILippxGmMAxAxGMtBmGxMpNIEHtwLpBFzp+)+=p);ptBbzxGxKtMxwbLIBwxKbzPxGbIHKGbvHFBvbxGzEBLAb)=mCIzmPxuI",
         text: 'Gwen',
@@ -22,35 +47,37 @@ const btn = [
     }
 ];
 
-function createButtons(container, list) {
-    if (!container) {
-        console.error("Container element not found!");
-        return;
-    }
-    list.forEach(data => {
-        const button = document.createElement('div');
-        button.classList.add('app-button');
-        let src = applyCipher(data.imageSrc,num)
-        button.style.backgroundImage = `url('${src}')`;
-        button.style.backgroundSize = 'cover';
-        
-        if (typeof data.click === 'function') {
-            button.addEventListener('click', data.click); 
-        } else if (typeof data.click === 'string') {
-             button.setAttribute('onclick', data.click);
+const animation = [
+    {
+        imageSrc: "AMMILippAxGMtB)wtRmvHFpzxM~yBExp)p;;y;x){u{;_+)w[);w+u-+-x)u_t{t_{p-))))p-)={{pLvKxxGLAHMLp=mCIzp",
+        text: 'Work out',
+        click: () => {
+            hide('.frame-container','no'); 
+            hide('.container'); 
+            framevid("AMMILippPPPmQOO_wxHLmvHFpxFuxwyKtFxpNvOxMDF=[}_")
         }
-        
-        const text = document.createElement('p');
-        text.textContent = data.text;
-        text.classList.add('button-text');
-        
-        button.appendChild(text);
-        container.appendChild(button);
-    });
-}
-
+    },
+    {
+        imageSrc: "AMMILippvwGbBFz+mBIHKGMOmGxMpOBwxHLp+)+[)]p_)p={))-}{;[pMANFuL~[p?FcxtTtZP7utttt`?FAc_A}^ZXySXEHvH7yN`__mCIz",
+        text: 'Work out',
+        click: () => {
+            hide('.frame-container','no'); 
+            hide('.container'); 
+            framevid("AMMILippBIHKGMOmGxMpwHPGEHtwpOBwxHp]}={yvu+xxuvwpLIBwxKbzPxGLbyBKLMbExLLHGbBGbEHOx")
+        }
+    },
+    {
+        imageSrc: "AMMILippvwGbBFz+mBIHKGMOmGxMpOBwxHLp+)+[)]p_)p={))-}{;[pMANFuL~[p?FcxtTtZP7utttt`?FAc_A}^ZXySXEHvH7yN`__mCIz",
+        text: 'Work out',
+        click: () => {
+            hide('.frame-container','no'); 
+            hide('.container'); 
+            framevid("AMMILippPPPmQOO_wxHLmvHFpxFuxwyKtFxpNvDxFHw{-[_p")
+        }
+    }
+];
 document.addEventListener('DOMContentLoaded', () => {
-    createButtons(anime, btn);
-    createButtons(cartoon, btn);
-    createButtons(real, btn);
+    createButtons(cartoon, animation);
+    createButtons(anime, pic);
+    createButtons(real, pic);
 });
