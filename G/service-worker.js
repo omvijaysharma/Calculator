9671184070d882bcb69275f366b290140f6bcbb8
Vijay Sharma <157729319+omvijaysharma@users.noreply.github.com>
2025-11-12 13:47:43 +0530

    Create service-worker.js

---
+++ G/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('calculator-app-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                './index.html',
+                './style.css',
+                './js/script.js',
+                './src/font.ttf'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
