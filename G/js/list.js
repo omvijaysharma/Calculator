97d4d3d436eb2a151b15d9997ad4f0338a8713e9
Vijay Sharma <157729319+omvijaysharma@users.noreply.github.com>
2025-11-16 11:58:56 +0530

    Update list.js

--- G/js/list.js
+++ G/js/list.js
@@ -1,28 +1,19 @@
-// gwen
+function serialSet(list,fun,n){
+    for(let i = 1; i <= n; i++) {
+        list.push(fun(i.toString().padStart(2, '0')));
+    }
+}
+// gwen ======================================================================
 function gwenFun(no) {
     let all = applyCipher("AMMILippxGmMAxAxGMtBmGxMpNIEHtwLpBFzp+)+=p);ptBbzxGxKtMxwbLIBwxKbzPxGbIHKGbvHFBvbxGzEBLAb",num) + no + applyCipher("mCIzmPxuI",num)
     return all
 }
-
-let gwen = [
-    gwenFun("01"),
-    gwenFun("02"),
-    gwenFun("03"),
-    gwenFun("04"),
-    gwenFun("05"),
-    gwenFun("06"),
-    gwenFun("07"),
-    gwenFun("08"),
-    gwenFun("09"),
-    gwenFun("10"),
-    gwenFun("11"),
-];
-// girl
+let gwen = [];    serialSet(gwen,gwenFun,11)
+// girl ======================================================================
 function girlFun(no) {
     let all = applyCipher("AMMILippK-=QRSmubvwGmGxMpIHLMLp",num) + no + applyCipher("mLFtEEmCIz",num)
     return all
 }
-
 let girl = [
     girlFun("458/458382/458382"),
     girlFun("459/459007/459007"),
@@ -37,83 +28,46 @@ let girl = [
     girlFun("458/458651/458651"),
     applyCipher("AMMILippKNEx-=OtNEMmvHFpIHLMLp_)+_p_)+_+}_p_)+_+}_mLFtEEmCIz",num)
 ]
-// white 
+// white ======================================================================
 function whiteFun(no) {
     let all = applyCipher("AMMILippxGmMAxAxGMtBmGxMpNIEHtwLpBFzp+)+=p)}ptBbzxGxKtMxwbGBxKbtNMHFtMtbIHKGbvHFBvbxGzEBLAb",num) + no + applyCipher("mCIzmPxuI",num)
     return all
 }
-let white = [
-    whiteFun("01"),
-    whiteFun("02"),
-    whiteFun("03"),
-    whiteFun("04"),
-    whiteFun("05"),
-    whiteFun("06"),
-    whiteFun("07"),
-    whiteFun("08"),
-    whiteFun("09"),
-    whiteFun("10"),
-    whiteFun("11"),
-    whiteFun("12"),
-    whiteFun("13"),
-    whiteFun("14"),
-    whiteFun("15"),
-    whiteFun("16"),
-    whiteFun("17"),
-    whiteFun("18"),
-    whiteFun("19"),
-    whiteFun("20"),
-    whiteFun("21"),
-    whiteFun("22"),
-    whiteFun("23"),
-    whiteFun("24"),
-    whiteFun("25"),
-    whiteFun("26"),
-    whiteFun("27"),
-    whiteFun("28"),
-    whiteFun("29"),
-    whiteFun("30"),
-    whiteFun("31"),
-    whiteFun("32"),
-    whiteFun("33"),
-    whiteFun("34"),
-    whiteFun("35"),
-    whiteFun("36"),
-    whiteFun("37"),
-    whiteFun("38"),
-    whiteFun("39"),
-    whiteFun("40"),
-    whiteFun("41"),
-    whiteFun("42"),
-    whiteFun("43"),
-    whiteFun("44"),
-    whiteFun("45"),
-    whiteFun("46"),
-    whiteFun("47"),
-    whiteFun("48"),
-    whiteFun("49"),
-    whiteFun("50"),
-    whiteFun("51"),
-    whiteFun("52"),
-    whiteFun("53"),
-    whiteFun("54"),
-    whiteFun("55"),
-    whiteFun("56"),
-    whiteFun("57"),
-    whiteFun("58"),
-    whiteFun("59"),
-    whiteFun("60"),
-    whiteFun("61"),
-    whiteFun("62"),
-    whiteFun("63"),
-    whiteFun("64"),
-    whiteFun("65"),
-    whiteFun("66"),
-    whiteFun("67"),
-    whiteFun("68"),
-    whiteFun("69"),
-    whiteFun("70"),
-    whiteFun("71"),
-    whiteFun("72"),
-    whiteFun("73")
-];
+let white = [];    serialSet(white,whiteFun,73)
+// rabbit ======================================================================
+
+function rabbitFun(no) {
+    let all = applyCipher("AMMILippxGmMAxAxGMtBmGxMpNIEHtwLpBFzp+)+=p)}ptBbzxGxKtMxwbLIBwxKbPHFtGbIHKGbvHFBvbxGzEBLAb",num) + no + applyCipher("mCIzmPxuI",num)
+    return all
+}
+let rabbit = [];    serialSet(rabbit,rabbitFun,61)
+// morning bath ============================================================
+function morningBathFun(no) {
+    let all = applyCipher("AMMILippLtMHKNmLtDNAxGMtBmGxMptGBFxLpKHLABwxKxpRNDBbHGLxGbtLBFIExGBGzxGb",num) + no + applyCipher("mPxuI",num)
+    return all
+}
+let morningBath = [];    serialSet(morningBath,morningBathFun,24)
+// cow ======================================================================
+function cowFun(no) {
+    let all = applyCipher("AMMILippLNzNKNmLtDNAxGMtBmGxMptGBFxLpKHLABwxKxptERtbvHPbuBDBGBbxLHSxb",num) + no + applyCipher("mPxuI",num)
+    return all
+}
+let cow = [];    serialSet(cow,cowFun,30)
+// night cow ======================================================================
+function sleepCowFun(no) {
+    let all = applyCipher("AMMILippLtMHKNmLtDNAxGMtBmGxMptGBFxLpKHLABwxKxptERtbFxzNFB+-b",num) + no + applyCipher("mPxuI",num)
+    return all
+}
+let sleepCow = [];    serialSet(sleepCow,sleepCowFun,30)
+// swim cow ======================================================================
+function swimCowFun(no) {
+    let all = applyCipher("AMMILippLNzNKNmLtDNAxGMtBmGxMptGBFxLpKHLABwxKxptERtbLPBFLNBMbFtvABGtb",num) + no + applyCipher("mPxuI",num)
+    return all
+}
+let swimCow = [];    serialSet(swimCow,swimCowFun,22)
+// sport cow ======================================================================
+function sportCowFun(no) {
+    let all = applyCipher("AMMILippLNzNKNmLtDNAxGMtBmGxMptGBFxLpKHLABwxKxptERtbLIHKMLPxtKbFtvABGtb",num) + no + applyCipher("mPxuI",num)
+    return all
+}
+let sportCow = [];    serialSet(sportCow,sportCowFun,30)
